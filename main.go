e1017307d175fc87b8bbe367cc6f1928cb819fa7
Masahiro Furudate <178inaba.git@gmail.com>
2020-03-14 02:10:06 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	var count int
+	var timeout time.Duration
+	var webhookRawurl string
+
+	flag.IntVar(&count, "count", 1, "Count.")
+	flag.DurationVar(&timeout, "timeout", 5*time.Second, "Timeout.")
+	flag.StringVar(&webhookRawurl, "webhook", "", "Slack webhook URL.")
+
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) == 0 {
+		log.Fatal("You must specify a target url.")
+	}
+	targetRawurl := args[0]
+
+	if _, err := url.ParseRequestURI(targetRawurl); err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	req, err := http.NewRequest(http.MethodHead, targetRawurl, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var i int
+	for {
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		resp, err := http.DefaultClient.Do(req.Clone(ctx))
+		if err != nil {
+			i = 0
+			log.Print(err)
+			continue
+		}
+
+		if resp.StatusCode == http.StatusOK {
+			i++
+			if i < count {
+				log.Printf("OK count: %d.", i)
+				continue
+			}
+
+			cancel()
+			break
+		}
+
+		i = 0
+		log.Print(resp.Status)
+	}
+
+	msg := fmt.Sprintf("%s: OK!", targetRawurl)
+	log.Print(msg)
+	if err := slack.PostWebhookContext(ctx, webhookRawurl, &slack.WebhookMessage{Text: msg}); err != nil {
+		log.Fatal(err)
+	}
+}
